947322c083967a88857525d56c478ccaed8d70fe
Ali Al Rafei <alielrafei.fcih@gmail.com>
2026-08-18 04:47:30 +0300

    fix: Mark AggregatorController as deprecated

--- src/main/java/com/acorp/jvminsight/httpserver/controller/AggregatorController.java
+++ src/main/java/com/acorp/jvminsight/httpserver/controller/AggregatorController.java
@@ -5,6 +5,7 @@ import java.net.HttpURLConnection;
 import java.net.URL;
 import java.nio.charset.StandardCharsets;
 
+@Deprecated
 public final class AggregatorController {
   private AggregatorController() {}
 
